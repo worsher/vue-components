63421d6ec2adf455794a5e72f99b9931df4e39c4
wangfeiran <wangfeiran@qiyi.com>
2019-07-02 11:23:48 +0800

    第一个ts文件

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @authors Wangfeiran (wangfeiran@qiyi.com)
+ * @date    2019-06-17 16:37:11
+ * @version 1.0.0
+ * @desc 加载整个组件库
+ */
+import Vue from 'vue'
+
+interface Components {
+	[key : string] : any
+}
+const components : Components = {}
+
+export default {
+	install (vue: typeof Vue){
+		for(var k  in components){
+			let value : any = components[k]
+			vue.component(value.name,value)
+		}
+		
+	}
+}
---
+++ src/views/Example.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<div class="title">
+			模块测试页面
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name : "example"
+	}
+</script>
